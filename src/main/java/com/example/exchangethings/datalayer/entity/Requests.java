94a06b681b43a8a421869114fa7f6867389a1254
mlyagusheva <green.day.forever@mail.ru>
2022-11-15 00:24:03 +0300

    add entities

---
+++ src/main/java/com/example/exchangethings/datalayer/entity/Requests.java
@@ -0,0 +1,10 @@
+package com.example.exchangethings.datalayer.entity;
+
+public class Requests {
+    private int RequestID;
+    private String publicationTime;
+    private int requestStatusID;
+    private String commentReceiver;
+    private int itemSenderID;
+    private int itemReceiverID;
+}
---
+++ src/main/java/com/example/exchangethings/datalayer/entity/UserStatus.java
@@ -0,0 +1,28 @@
+package com.example.exchangethings.datalayer.entity;
+
+public class UserStatus {
+
+    private int userStatusID;
+    private String userStatusName;
+
+    public UserStatus(int userStatusID, String userStatusName) {
+        this.userStatusID = userStatusID;
+        this.userStatusName = userStatusName;
+    }
+
+    public int getUserStatusID() {
+        return userStatusID;
+    }
+
+    public void setUserStatusID(int userStatusID) {
+        this.userStatusID = userStatusID;
+    }
+
+    public String getUserStatusName() {
+        return userStatusName;
+    }
+
+    public void setUserStatusName(String userStatusName) {
+        this.userStatusName = userStatusName;
+    }
+}
