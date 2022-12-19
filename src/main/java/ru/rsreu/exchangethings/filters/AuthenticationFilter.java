6ec1aa3eca518f56af6b70c178096e24eb151a02
micla <adraydante@gmail.com>
2022-12-19 21:22:20 +0300

    remove countur

--- src/main/java/ru/rsreu/exchangethings/filters/AuthenticationFilter.java
+++ src/main/java/ru/rsreu/exchangethings/filters/AuthenticationFilter.java
@@ -19,8 +19,6 @@ public class AuthenticationFilter implements Filter {
     Logger logger = Logger.getLogger(this.getClass().getName());
     TokenStorage tokenService = SecurityService.instance;
 
-    String contour;
-
     private Object getToken(HttpServletRequest request) {
         return request
                 .getSession()
@@ -39,19 +37,17 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) {
-        contour = filterConfig.getInitParameter("contour");
+
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if (!contour.equals("test")) {
-            String token = (String) this.getToken(httpRequest);
-            if ((token == null || !tokenService.tokenExist(token)) && (!this.isLoginCommand(request))) {
-                logger.info("filter exception AuthenticationException");
-                request.setAttribute("error", "Ошибка доступа");
-                throw new AuthenticationException();
-            }
+        String token = (String) this.getToken(httpRequest);
+        if ((token == null || !tokenService.tokenExist(token)) && (!this.isLoginCommand(request))) {
+            logger.info("filter exception AuthenticationException");
+            request.setAttribute("error", "Ошибка доступа");
+            throw new AuthenticationException();
         }
         chain.doFilter(request, response);
     }
